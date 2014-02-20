324532360baa2996e8db172243b1a0f70faa4483
Berg <dberg@engr-131-16.eastern.ewu.edu>
2014-02-20 14:30:42 -0800

    Hola

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,14 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
+	var msg1 = 'Hello!';
+	var msg2 = 'Hola!';
+	var msg3 = 'Bonjour!';
+
+	$('h1').text(msg2);
+
+
+
 	console.log('Page Loaded. Lets Do this!');
 
 }); 
